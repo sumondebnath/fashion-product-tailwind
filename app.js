9e301e3a535ea2bca2f851ea8228b13cc9c55685
sumondebnath <debnathsumon1708@gmail.com>
2024-06-07 16:41:11 +0600

    responsive

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 
+
+
 const toggler = document.getElementById("togle");
 const menus = document.getElementById("menu");
+const banner_text =document.getElementById("banner-text");
 
 toggler.addEventListener("click", ()=>{
-    menus.classList.toggle("block");
+    menus.classList.toggle("hidden");
+    banner_text.classList.toggle("hidden");
+
 });
+
+
+// const handleToggle = (event) =>{
+//     event.preventDefault()
+//     const menu = document.getElementById("menu");
+//     console.log("hello");
+//     menu.classList.toggle("block");
+// };
